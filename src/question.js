b01f2d0da119e6ab5da99c04d7f6be1d9690962c
Daniela <daniela.primacov@yahoo.com>
2025-02-06 19:52:41 +0100

    First Day Tasks Resolved

--- src/question.js
+++ src/question.js
@@ -1,7 +1,23 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+  // YOUR CODE HERE:
+  //
+  // 1. constructor (text, choices, answer, difficulty)
+  constructor(text, choices, answer, difficulty) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+    this.difficulty = difficulty;
+  }
 
-    // 2. shuffleChoices()
+  // 2. shuffleChoices()
+  shuffleChoices() {
+    let currentElement;
+    for (let i = this.choices.length - 1; i > 0; i--) {
+      let rand = Math.floor(Math.random() * (i + 1));
+      currentElement = this.choices[i];
+      this.choices[i] = this.choices[rand];
+      this.choices[rand] = currentElement;
+    }
+    return this.choices;
+  }
 }
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,52 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  // YOUR CODE HERE:
+  //
+  // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  // 2. getQuestion()
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
 
-    // 4. shuffleQuestions()
+  // 3. moveToNextQuestion()
+  moveToNextQuestion() {
+    this.currentQuestionIndex += 1;
+  }
 
-    // 5. checkAnswer(answer)
+  // 4. shuffleQuestions()
+  shuffleQuestions() {
+    let currentElement;
+    for (let i = this.questions.length - 1; i > 0; i--) {
+      let rand = Math.floor(Math.random() * (i + 1));
+      currentElement = this.questions[i];
+      this.questions[i] = this.questions[rand];
+      this.questions[rand] = currentElement;
+    }
+    return this.questions;
+  }
 
-    // 6. hasEnded()
+  // 5. checkAnswer(answer)
+  checkAnswer(answer) {
+    if (answer) {
+      this.correctAnswers += 1;
+    }
+  }
+
+  // 6. hasEnded()
+  hasEnded() {
+    if (this.currentQuestionIndex === this.questions.length) {
+      return true;
+    } else if (this.currentQuestionIndex < this.questions.length) {
+      return false;
+    }
+  }
 }
+
+
